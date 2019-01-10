5e82e54aa415405859ac7d1869212d2dfe520db9
Darya Reznichenko <homewinee@gmail.com>
2019-01-11 00:53:47 +0300

    first commit

---
+++ firstlab/Make/file2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+//<man> This function prints \'World\'
+void func1()
+{
+  printf("World !");
+}
---
+++ firstlab/Make/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "file2.h"
+#include "file3.h"
+
+//<man> This function prints hello and calls other functions
+int main()
+{
+  printf("Hello ");
+  func1();
+  func2();
+
+  
+  return 0;
+}
